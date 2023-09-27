640e793b6ea9b951bf3953143618b24bcf65f257
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-09-27 20:22:52 +0100

    [GENERAL IMPROVEMENTS] Improve code, add stuff
    This will take a while.

--- src/main/java/me/brett/tutorial/Tutorial.java
+++ src/main/java/me/brett/tutorial/Tutorial.java
@@ -7,22 +7,32 @@ import me.brett.tutorial.module.Module;
 import me.brett.tutorial.module.ModuleManager;
 import org.lwjgl.opengl.Display;
 
+/**
+ * The main class for the tutorial client.
+ * This will load the module manager, set the client version, among other things.
+ *
+ * @author BrettHax
+ * @since 10/04/2023
+ */
 public class Tutorial {
 
     private static Tutorial singleton;
-    public String name = "Tutorial", version = "0.0.1";
+
+    public static String NAME = "Tutorial";
+    public static String VERSION = "0.0.1";
+
     public ModuleManager moduleManager = new ModuleManager();
     public EventBus eventBus = new EventBus();
 
     public static Tutorial getInstance() {
-        if(singleton == null) {
+        if (singleton == null) {
             singleton = new Tutorial();
         }
         return singleton;
     }
 
     public void init() {
-        Display.setTitle(name + " Version: " + version);
+        Display.setTitle(NAME + VERSION);
         moduleManager.init();
         eventBus.register(this);
     }
